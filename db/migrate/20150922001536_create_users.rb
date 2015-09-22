a307573bb607029bd6525ff09b7eb44885e55f94
miokawai <miokawai1210@gmail.com>
2015-09-22 08:42:48 +0000

    add sign sign up and user detail

--- db/migrate/20150922001536_create_users.rb
+++ db/migrate/20150922001536_create_users.rb
@@ -1,10 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :miokawai
-      t.string :miokawai1210@gmail.com
-      t.string :miokawai12101210
-
+      t.string :name
+      t.string :email
+      t.string :password_digest
       t.timestamps null: false
       t.index:email,unique: true
     end
