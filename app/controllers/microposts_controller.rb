0572a146b117571d60f1db38f00139427c152391
miokawai <miokawai1210@gmail.com>
2015-09-28 09:56:07 +0000

    add posting microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,9 +1,9 @@
 class MicropostsController < ApplicationController
-  before_action :logged_in_user, only: [:create]
-
+  before_action :logged_in_user, only: [:create, :destroy]
+  
   def create
-    @micropost = current_user.microposts.build(micropost_params)
-    if @micropost.save
+    @microposts = current_user.microposts.build(microposts_parmas)
+    if @microposts.save
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
@@ -11,9 +11,16 @@ class MicropostsController < ApplicationController
     end
   end
   
-  private
-  def micropost_params
-    params.require(:micropost).permit(:content)
+  def destroy
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    return redirect_to root_url if @micropost.nil?
+    @micropost.destroy
+    flash[:success] = "Micropost deleted"
+    redirect_to request.referrer || root_url
   end
+  
+private
+def microposts_parmas
+  params.require(:micropost).permit(:content)
 end
-
+end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,5 @@
 class StaticPagesController < ApplicationController
+  def home
+    @microposts = current_user.microposts.build if logged_in?
+  end
 end
