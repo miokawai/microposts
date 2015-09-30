abaf766e57cd2ba1cb31cc2bf3d260aed45c84cb
miokawai <miokawai1210@gmail.com>
2015-09-30 02:32:36 +0000

    first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
   def edit
-    @user = User.edit
-  end
+    unless  logged_in?
+     return redirect_to root_url
+   end
+   @user = User.find(params[:id])
+ end
     
   def show # 追加
    @user = User.find(params[:id])
@@ -15,8 +18,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+       session[:user_id] = @user.id
        flash[:success] = "Welcome to the Sample App, #{@user.name}!!"
-      redirect_to @user # ここを修正
+      redirect_to @user 
     else
       render 'new'
     end
@@ -24,6 +28,9 @@ class UsersController < ApplicationController
 
  def update
     @user = User.find(params[:id])
+    if @user != current_user
+      return redirect_to root_url
+    end
     if @user.update(user_params)
       flash[:success] = "プロフィールを更新しました"
       redirect_to @user
@@ -31,9 +38,17 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-  def edit
+  
+  def followings
     @user = User.find(params[:id])
+    @followings = @user.following_users
   end
+  
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.follower_users
+  end
+  
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :age, :comment, :website)
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ActiveRecord::Base
                   format: { with: VALID_EMAIL_REGEX },
                    uniqueness: { case_sensitive: false }
   has_secure_password
+  validates :age, presence:false,numericality: {only_integer:true, greater_than_or_equal_to: 0,less_than: 100}, on: :update
+  validates :comment, presence: false, length: { maximum:100, minimum:0 }, on: :update
+  VALID_EMAIL_REGEX = /\Ahttp/
   has_many :microposts
   has_many :following_relationships, class_name: "Relationship",
                                      foreign_key: "follower_id",
